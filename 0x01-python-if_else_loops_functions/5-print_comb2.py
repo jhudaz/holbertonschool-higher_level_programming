0f9f1b547924bc71a7d48676c71dbe3892257916
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-09-10 14:16:46 -0500

    fix: change format at 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{:02d}, ".format(i), end="")
-    if i == 99:
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
         print("{:02d}".format(i))
+
