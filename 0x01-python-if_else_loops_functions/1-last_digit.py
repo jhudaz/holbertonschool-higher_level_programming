4c5ea12a3d96c7b93e36ca186338238df2b90ecf
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-09-10 21:03:37 -0500

    fix: removing hard code and additional spaces

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-number = 5247
+number = random.randint(-10000, 10000)
 str1 = " and is greater than 5"
 str2 = " and is 0"
 str3 = " and is less than 6 and not 0"
@@ -9,8 +9,8 @@ if number < 0:
 else:
     last = number % 10
 if last > 5:
-    print("Last digit of {} is  {}".format(number, last) + str1)
+    print("Last digit of {} is {}".format(number, last) + str1)
 elif last == 0:
-    print("Last digit of {} is  {}".format(number, last) + str2)
+    print("Last digit of {} is {}".format(number, last) + str2)
 else:
-    print("Last digit of {} is  {}".format(number, last) + str3)
+    print("Last digit of {} is {}".format(number, last) + str3)
