08f6eaf727dabc3bed452e524b5b54e592f934ee
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-09-10 21:00:56 -0500

    fix: change function structure

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
+number = 5247
+str1 = " and is greater than 5"
+str2 = " and is 0"
+str3 = " and is less than 6 and not 0"
 if number < 0:
     last = number % -10
 else:
     last = number % 10
 if last > 5:
-    print("Last digit of {} is  {} and is greater than 5".format(number, last))
+    print("Last digit of {} is  {}".format(number, last) + str1)
 elif last == 0:
-    print("Last digit of {} is  {} and is 0".format(number, last))
+    print("Last digit of {} is  {}".format(number, last) + str2)
 else:
-    print("Last digit of {} is  {} and is \
-less than 6 and not 0".format(number, last))
+    print("Last digit of {} is  {}".format(number, last) + str3)
