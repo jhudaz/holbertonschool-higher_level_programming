3d5b7683e7a333075b7343f4fbef46286927d5c3
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-09-12 15:04:28 -0500

    fix: change the import statetment

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as add
+from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
 
-    print("{} + {} = {}".format(a, b, add.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
