39b67e482a1a8c5ec10f24a091c1af26bc57701d
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-09-13 00:48:53 -0500

    fix: change in the structure

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,9 +3,8 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-    for i in range(4, 6):
-        c = add(c, i)
-    return c
-
-    return sub(a, b);return ;return
-    
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
