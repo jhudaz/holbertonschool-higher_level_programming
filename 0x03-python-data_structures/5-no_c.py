df529b080fc9ab96a1ea856b8cb7a04e55b1aa39
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-09-17 20:00:38 -0500

    feature: changes in the structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,8 @@
 def no_c(my_string):
     new_str = ""
     for i in range(len(my_string)):
-        if my_string[i] != 'C' or my_string[i] != 'c':
-            new_str = my_string[i]
-        else:
+        if my_string[i] == 'C' or my_string[i] == 'c':
             pass
-
+        else:
+            new_str += my_string[i]
     return new_str
