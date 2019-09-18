740a1e69bd0ccc187f5f19a596698d42beda2209
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-09-17 20:51:23 -0500

    fix: adding the format to print integers in the print function

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
-        print()
+        print("")
     else:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end="")
                 if j != len(matrix[i]) - 1:
                     print(" ", end="")
-            print()
+            print("")
