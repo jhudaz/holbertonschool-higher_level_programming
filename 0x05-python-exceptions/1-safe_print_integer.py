48d75b41977143d040fdd30ea729a627af25551c
jandres <jandres.vr@hotmail.com>
2019-09-30 13:28:51 -0500

    feature: changes inthe function

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return True
     except ValueError:
         return False
