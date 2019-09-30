5c2e8a44933847c959d08504d989cfbc4de98092
jandres <jandres.vr@hotmail.com>
2019-09-30 14:56:11 -0500

    feature: create a new list using values from two lists handling excepts

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    list = [0] * list_length
+    for i in range(list_length):
+        try:
+            list[i]= my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+    return list
