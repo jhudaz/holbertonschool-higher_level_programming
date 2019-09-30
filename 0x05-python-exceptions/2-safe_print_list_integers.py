bd6c86d897ece70cb8ad47a63df7f70b1f98a264
jandres <jandres.vr@hotmail.com>
2019-09-30 14:06:47 -0500

    fix: changing the structure for th iteration

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for value in my_list[:x]:
+    for i in range(x):
         try:
-            print("{:d}".format(value), end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
             pass
