4be4ff6a625f6a8787d401b1d27abbb10c9f7da2
jandres <jandres.vr@hotmail.com>
2019-10-03 00:42:23 -0500

    feature: class that handle errors and use getter and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
