5416d89ea0382dfdb8b760554d772a55cd2d56ed
jandres <jandres.vr@hotmail.com>
2019-10-04 10:21:34 -0500

    feature: doctest for a function that add two numbers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+"add" module.
+
+add module supplies one function, add_integer().  For example,
+
+>>> add_integer(2, 98)
+100
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Return the add from two numbers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    return int(a + b)
