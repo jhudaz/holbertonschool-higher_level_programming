e548f63ddff2efd737f2a05761165d9b935e8d0c
Jaime Velez <jandres.vr@hotmail.com>
2019-10-05 00:45:53 -0500

    feature: function that gives and indentation to a string

--- 0x07-python-test_driven_development/5-main.py
+++ 0x07-python-test_driven_development/5-main.py
@@ -4,7 +4,7 @@ text_indentation = __import__('5-text_indentation').text_indentation
 text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
 Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
 Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Plus semper voluptatis?.: Teneo, inquit, finem illi videri nihil dolere. \
 Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
 rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
 stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,10 +2,14 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    for i in range(len(text)):
-
-        if text[i] == '.' or text[i] == '?' or text[i] == ':':
-            print(text[i], end="")
-            print('\n')
-        else:
-            print(text[i], end="")
+    newStr1 = text.replace('.', '.\n\n')
+    newStr2 = newStr1.replace(':', ':\n\n')
+    newStr3 = newStr2.replace('?', '?\n\n')
+    lst = newStr3.split('\n\n')
+    if len(lst[-1]) == 0:
+        del(lst[-1])
+    for i in lst:
+        print(i.lstrip(), end="")
+        if i == lst[-1] and i != ':' and i != '.' and i != '?':
+            break
+        print('\n')
