76881c83eba5e43cd39af90a5a01ae5835d46413
jandres <jandres.vr@hotmail.com>
2019-10-07 12:13:38 -0500

    fix: changes in the setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,6 +18,7 @@ class Rectangle:
             raise TypeError('width must be an integer')
         if width < 0:
             raise ValueError('width must be >= 0')
+        self.__width = width
 
     @height.setter
     def height(self, height):
@@ -25,3 +26,4 @@ class Rectangle:
             raise TypeError('height must be an integer')
         if height < 0:
             raise ValueError('height must be >= 0')
+        self.__height = height
