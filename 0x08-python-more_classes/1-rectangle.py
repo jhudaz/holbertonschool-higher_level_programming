a6a56c58aab809cbc73183cc8f97c72460189fb4
jandres <jandres.vr@hotmail.com>
2019-10-07 23:10:15 -0500

    fix: changing the type of attribute from private to public

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -13,17 +13,17 @@ class Rectangle:
         return self.__height
 
     @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
+    def width(self, width):
+        if not isinstance(width, int):
             raise TypeError('width must be an integer')
-        if value < 0:
+        if width < 0:
             raise ValueError('width must be >= 0')
-        self.__width = value
+        self.__width = width
 
     @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
+    def height(self, height):
+        if not isinstance(height, int):
             raise TypeError('height must be an integer')
-        if value < 0:
+        if height < 0:
             raise ValueError('height must be >= 0')
-        self.__height = value
+        self.__height = height
