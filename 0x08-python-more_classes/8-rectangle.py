b0dcb7291ca1a494c4445a9b152d8726cd7d0a21
jandres <jandres.vr@hotmail.com>
2019-10-07 23:33:16 -0500

    fix: changes in the sintax of python

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -66,7 +66,7 @@ class Rectangle:
             raise TypeError('rect_1 must be an instance of Rectangle')
         if not isinstance(rect_2, Rectangle):
             raise TypeError('rect_2 must be an instance of Rectangle')
-        if rect_1.area >= rect_2.area:
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
