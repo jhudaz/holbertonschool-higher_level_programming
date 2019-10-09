1c244a481bbedf37aa44f36603de33ebe8970588
jandres <jandres.vr@hotmail.com>
2019-10-09 18:26:01 -0500

    feature: hanges

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,14 +13,8 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    newStr1 = text.replace('.', '.\n\n')
-    newStr2 = newStr1.replace(':', ':\n\n')
-    newStr3 = newStr2.replace('?', '?\n\n')
-    lst = newStr3.split('\n\n')
-    if len(lst[-1]) == 0:
-        del(lst[-1])
-    for i in lst:
-        print(i.lstrip(), end="")
-        if i == lst[-1] and i != ':' and i != '.' and i != '?':
-            break
-        print('\n')
+    t = text.replace('.', '.\n\n')
+    t = t.replace(':', ':\n\n')
+    t = t.replace('?', '?\n\n')
+    t = t.replace('\n ', '\n')
+    print(t)
