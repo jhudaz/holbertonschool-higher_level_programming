6d80abefe414733da20c200a98f47c57e13ac020
jandres <jandres.vr@hotmail.com>
2019-10-16 11:20:20 -0500

    feature: function that return the count of lines in a file

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def number_of_lines(filename=""):
+    """ number_of_lines
+    Args:
+        filename (path): file path
+
+    Returns:
+        Number of lines
+    """
+    with open(filename, encoding='utf-8') as file:
+        data = file.read()
+        count = 0
+        for i in range(len(data)):
+            if data[i] == "\n":
+                count += 1
+        return count
