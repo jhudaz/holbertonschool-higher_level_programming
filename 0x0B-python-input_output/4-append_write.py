03397c8d45dd8725b8d963e966298d742efb5e07
jandres <jandres.vr@hotmail.com>
2019-10-16 12:45:12 -0500

    feature: append a string to a file

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def append_write(filename="", text=""):
+    """ write_file
+    Args:
+        filename (path): file path
+        text (str): string
+
+    Returns:
+        write a string in a new file
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        print(filename)
+        return file.write(text)
