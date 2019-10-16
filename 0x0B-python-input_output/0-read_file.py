3feaebfb13184d10009a88b2ed52a012f50c213f
jandres <jandres.vr@hotmail.com>
2019-10-16 10:56:59 -0500

    update: documentation

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """ read-file print the file content
+    Args:
+        filename (path): file path
+
+    Returns:
+        Nothing
+    """
+    with open(filename, encoding='utf-8') as file:
+        data = file.read()
+    print(data)
