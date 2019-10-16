480b0865475395fe464cafbb9b47bf54938ae543
jandres <jandres.vr@hotmail.com>
2019-10-16 12:13:47 -0500

    feature: print n lines ofa file

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    """ read_lines
+    Args:
+        filename (path): file path
+        nb_lines (int): number of lines
+
+    Returns:
+        read the lines a print to stdout
+    """
+    with open(filename, encoding='utf-8') as file:
+        if nb_lines <= 0:
+            data = file.read()
+            print(data)
+        else:
+            for i in range(nb_lines):
+                print(file.readline(), end="")
+    file.close()
