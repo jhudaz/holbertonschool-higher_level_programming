44cf253e4e7c050c8d62e472b4ecd992eb128411
jandres <jandres.vr@hotmail.com>
2019-10-16 12:29:14 -0500

    feature: write a string in a file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """ write_file
+    Args:
+        filename (path): file path
+        text (str): string
+
+    Returns:
+        write a string in a new file
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)
