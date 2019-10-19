5f050674f4887265e2e0c0d77cb39af8fb393f81
jandres <jandres.vr@hotmail.com>
2019-10-18 19:05:11 -0500

    feature: updat method added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,6 +100,19 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        """"""
+        """return a string with the data passed"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        if args[0] is not None:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
