f94eb6cbac007b947a4a34bd526d920f25bb7851
jandres <jandres.vr@hotmail.com>
2019-10-21 18:54:52 -0500

    feature: return the json string representation from a list

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """ model `Base`
 """
 
@@ -10,7 +11,7 @@ class Base:
 
     def __init__(self, id=None):
         """constructor
-        
+
         Keyword Arguments:
             id {int} -- (default: {None})
         """
@@ -18,19 +19,20 @@ class Base:
 
     @property
     def id(self):
-        """id getter
-        """
+        """id getter"""
         return self.__id
 
     @id.setter
     def id(self, value):
-        """id setter
-
-        Arguments:
-            value {int} -- new value
-        """
+        """id setter"""
         if value is not None:
             self.__id = value
         else:
             Base.__nb_objects += 1
             self.__id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
