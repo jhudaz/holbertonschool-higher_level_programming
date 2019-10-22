5d3ff66491096cb70ef40c4be17edc6e3c13c67b
jandres <jandres.vr@hotmail.com>
2019-10-22 12:53:44 -0500

    feature: returns a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,6 +38,14 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """from_json_string"""
+        if json_string is None or json_string == []:
+            return []
+        else:
+            return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
@@ -51,3 +59,33 @@ class Base:
 
         with open(file, "w", encoding="utf-8") as fle:
             fle.write(content)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create """
+        if cls.__name__ == "Square":
+            newCls = cls(1, 0, 0)
+
+        if cls.__name__ == "Rectangle":
+            newCls = cls(1, 1, 0, 0)
+
+        newCls.update(**dictionary)
+        return newCls
+
+    @classmethod
+    def load_from_file(cls):
+        """from_json_string    return a string """
+        """create"""
+
+        """returns a list of instances"""
+        name = cls.__name__+".json"
+        lst = []
+        try:
+            with open(name, encoding="utf-8") as file:
+                data = file.read()
+                json_data = cls.from_json_string(data)
+                new = [cls.create(**obj) for obj in json_data]
+                return new
+
+        except FileNotFoundError:
+            return []
