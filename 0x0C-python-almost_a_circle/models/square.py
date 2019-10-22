c5e3cd018c5d18ea804d8f9f01468d7d634c11f8
jandres <jandres.vr@hotmail.com>
2019-10-22 15:38:20 -0500

    feature: changes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""model `Rectangle`"""
+"""model `Square`"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """class Square"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """constructor"""
         super().__init__(size, size, x, y, id)
