fe096051bdc9caea901ba172f0b03045bf8cf963
jandres <jandres.vr@hotmail.com>
2019-10-22 09:56:24 -0500

    feature: create a json file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 import json
-""" model `Base`
-"""
+""" model `Base`"""
 
 
 class Base:
-    """class `Base`
-    """
+    """class `Base`"""
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -31,8 +29,25 @@ class Base:
             Base.__nb_objects += 1
             self.__id = Base.__nb_objects
 
+    """methods"""
+
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        file = cls.__name__ + ".json"
+        content = ""
+        lst = []
+        for obj in list_objs:
+            lst.append(obj.to_dictionary())
+
+        content = cls.to_json_string(lst)
+
+        with open(file, "w", encoding="utf-8") as fle:
+            fle.write(content)
