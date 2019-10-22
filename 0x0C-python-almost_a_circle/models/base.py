e58444837d97fb5df640aa7d6acf729a9b08041b
jandres <jandres.vr@hotmail.com>
2019-10-22 14:45:10 -0500

    fix: removing getter and setter of id attribute

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = self.__nb_objects
 
     """methods"""
 
