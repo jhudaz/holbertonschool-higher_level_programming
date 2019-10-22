9e5f344f5beca00b1391e96b0f046c904e50218d
jandres <jandres.vr@hotmail.com>
2019-10-22 15:16:36 -0500

    feature: changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import json
 """ model `Base`"""
+import json
 
 
 class Base:
@@ -8,18 +8,12 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """constructor
-
-        Keyword Arguments:
-            id {int} -- (default: {None})
-        """
+        """constructor"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
-
-    """methods"""
 
     @staticmethod
     def to_json_string(list_dictionaries):
