399ab2e808683da7e93fa62f4fc876503102e05b
jandres <jandres.vr@hotmail.com>
2019-11-26 01:14:33 -0500

    update: changes

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,8 +5,4 @@ CREATE TABLE IF NOT EXISTS second_table (
         score INT
     );
 INSERT INTO second_table(id, name, score) 
-VALUES
-(1, "Johnl", 10),
-(2, "Alex", 3),
-(3, "Bob", 14),
-(4, "George", 8);
+VALUES (1, "Johnl", 10), (2, "Alex", 3), (3, "Bob", 14),(4, "George", 8);
