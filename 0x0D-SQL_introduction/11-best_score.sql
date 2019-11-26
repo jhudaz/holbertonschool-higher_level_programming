8e468fe0dea1e2001a658bd683e0c13af6bb6b63
jandres <jandres.vr@hotmail.com>
2019-11-26 02:13:39 -0500

    update: changes in the where statement

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- select row where score is higher than 10
-SELECT score, name FROM second_table where score > 9 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score > 9 ORDER BY score DESC;
