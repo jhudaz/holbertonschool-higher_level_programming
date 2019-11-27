aed687fdd7f0a4850b9d01d8f73e90b29efd53b9
jandres <jandres.vr@hotmail.com>
2019-11-26 23:18:43 -0500

    feature: query  title and genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- lists all shows contained in the database
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+    LEFT JOIN tv_show_genres
+    ON tv_show_genres.show_id = tv_shows.id
+    LEFT JOIN tv_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name
