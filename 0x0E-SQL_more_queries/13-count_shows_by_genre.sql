6928953e51a091c3d43d9184d7df585d3017b6a6
jandres <jandres.vr@hotmail.com>
2019-11-26 21:51:51 -0500

    fix: uppercase for the key words

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in the database
-SELECT tv_genres.name as genre, COUNT(tv_genres.name) as number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows
 FROM tv_genres
     INNER JOIN tv_show_genres
     ON tv_show_genres.genre_id = tv_genres.id
