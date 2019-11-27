a832425dd18cd8e69c2bc3b691aae5a5374319b9
jandres <jandres.vr@hotmail.com>
2019-11-26 22:24:15 -0500

    fix: removing alias

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in the database
-SELECT DISTINCT tv_genres.name as name
+SELECT DISTINCT tv_genres.name
 FROM tv_genres
     INNER JOIN tv_show_genres
     ON tv_show_genres.genre_id = tv_genres.id
