7124647ccd2080f62d2a1026c93dee8756fa5539
jandres <jandres.vr@hotmail.com>
2019-11-26 21:49:01 -0500

    feature: count by categories

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- lists all shows contained in the database
+SELECT tv_genres.name as genre, COUNT(tv_genres.name) as number_of_shows
+FROM tv_genres
+    INNER JOIN tv_show_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+    INNER JOIN tv_shows
+    ON tv_shows.id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC
