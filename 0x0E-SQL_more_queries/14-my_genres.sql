94ce59c450b4cf610d3d92fc108899a65fc3d2f0
jandres <jandres.vr@hotmail.com>
2019-11-26 22:20:08 -0500

    feature: query

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- lists all shows contained in the database
+SELECT tv_genres.name
+FROM tv_genres
+    INNER JOIN tv_show_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+    INNER JOIN tv_shows
+    ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter"
