8b43c847ccfbe24a34a598b5a16d2e426d9319fd
jandres <jandres.vr@hotmail.com>
2019-11-26 22:52:12 -0500

    feature: another query

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- lists all shows contained in the database
+SELECT tv_shows.title
+FROM tv_shows
+    INNER JOIN tv_show_genres
+    ON tv_show_genres.show_id = tv_shows.id
+    INNER JOIN tv_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title
