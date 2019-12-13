63ff510cd58e6d7755d1479f5d78cd2e0deb3fd4
jandres <jandres.vr@hotmail.com>
2019-12-13 15:51:04 -0500

    feature: make a connectio to mysql from python

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" module """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db_conf = {
+        'host':     'localhost',
+        'user':     sys.argv[1],
+        'passwd':   sys.argv[2],
+        'db':       sys.argv[3],
+        'port':     3306
+    }
+    db = MySQLdb.connect(**db_conf)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
