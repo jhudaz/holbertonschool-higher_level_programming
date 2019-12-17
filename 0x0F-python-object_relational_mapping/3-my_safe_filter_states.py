6f400c810ef1a666570609a96c5c2b4124a5a24d
jandres <jandres.vr@hotmail.com>
2019-12-17 09:48:08 -0500

    feature: prevent sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" module """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db_conf = {
+        'host':     'localhost',
+        'user':     sys.argv[1],
+        'passwd':   sys.argv[2],
+        'db':       sys.argv[3],
+        'port':     3306
+    }
+    db = MySQLdb.connect(**db_conf)
+    cursor = db.cursor()
+    query = """ SELECT * FROM states
+                WHERE BINARY name = %s
+                ORDER BY id
+            """
+    cursor.execute(query, (sys.argv[4], ))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
