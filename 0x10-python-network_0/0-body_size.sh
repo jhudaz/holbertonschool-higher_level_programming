27bc0633e6019af7e8ebc041e4f4774d6576c0e4
jaime <jandres.vr@hotmail.com>
2020-01-06 21:11:15 +0000

    update: removing url and using argument #1 by default

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #content length in a header
-curl -sI "http://74ce65162028.38.hbtn-cod.io:5000/" | grep "Content-Length" | cut -d ":" -f2
+curl -sI $1 | grep "Content-Length" | cut -d ":" -f2
