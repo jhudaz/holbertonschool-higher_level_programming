37714bd1d61284016432e4f737a86121f89c2757
jaime <jandres.vr@hotmail.com>
2020-01-06 20:28:18 -0500

    feature: setting a header in a request

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -H 'X-HolbertonSchool-User-Id:98' $1
+curl -sH 'X-HolbertonSchool-User-Id:98' $1
