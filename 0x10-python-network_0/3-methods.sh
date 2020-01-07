1e45253d57ae75867194bf281f6e64f668290657
jaime <jandres.vr@hotmail.com>
2020-01-06 19:51:16 -0500

    feature: takes in a URL and displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #akes in a URL and displays all HTTP methods the server will accept.
-curl -siX OPTIONS  $1| grep "Allow" | cut -d ":" -f2
+curl -sIX OPTIONS  $1| grep "Allow" | cut -d ":" -f2
