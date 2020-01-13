f8027f9acae3fba8e8a451f549ed1c10cd519382
jaime <jandres.vr@hotmail.com>
2020-01-13 17:28:38 +0000

    feature: HTTPError

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
-#
+import urllib.parse
+import urllib.request
+import urllib.error
+from sys import argv
+# package to handle the requests and take the arguments from the user input
+
+
 if __name__ == "__main__":
+    url = argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+            print("{}".format(data.decode()))
+
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
