edad97d8099670b81b5b39bd90f61b3eccfc4fee
jaime <jandres.vr@hotmail.com>
2020-01-13 17:32:15 +0000

    fix: using tabs instead white spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ from urllib.request import urlopen
 if __name__ == "__main__":
     with urlopen('https://intranet.hbtn.io/status') as response:
         body = response.read()
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode()))
