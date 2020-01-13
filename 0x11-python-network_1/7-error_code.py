3645ec10891cd7296947b6df0f7be2ff6deb75c5
jaime <jandres.vr@hotmail.com>
2020-01-13 19:24:42 +0000

    feature: script that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-#
+# sends a request to the URL and displays the body of the response.
+import requests
+from sys import argv
+# packages to handle the request
+
+
 if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print("{}".format(r.text))
