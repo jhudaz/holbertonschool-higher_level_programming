2479cf5a1a2b80ba92a3a34d09d4afd593e953e1
jaime <jandres.vr@hotmail.com>
2020-01-13 17:48:53 +0000

    feature:script that fetches https://intranet.hbtn.io/status using requests package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-#
+# script that fetches https://intranet.hbtn.io/status using request package
+import requests
+# package to handle a request
+
+
 if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
