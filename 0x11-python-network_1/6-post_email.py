4aa3a67bf2fcc486b96f59b140b33568757e0ea1
jaime <jandres.vr@hotmail.com>
2020-01-13 19:06:25 +0000

    feature: script that takes in a URL and an email address, sends a POST request to the passed URL using requests package

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-#
+# sends a POST request to the passed URL with the email as a parameter
+import requests
+from sys import argv
+# packages to handle request
+
+
 if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+
+    values = {'email': email}
+
+    r = requests.post(url, data=values)
+
+    print("{}".format(r.text))
