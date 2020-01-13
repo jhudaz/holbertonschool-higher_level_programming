d968888a25d884b08bb68abd34259f8114e38ee9
jaime <jandres.vr@hotmail.com>
2020-01-13 22:31:40 +0000

    feature:script that takes in a string and sends a search request to the Star Wars API

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-#
+# script that takes in a string and sends a search request to the Star Wars API
+import requests
+from sys import argv
+# packages to handle the request
+
+
 if __name__ == "__main__":
+    url = "https://swapi.co/api/people/?search={}".format(argv[1])
+    r = requests.get(url).json()
+    data = r['results']
+    print("Number of results: {}".format(r["count"]))
+    for person in data:
+        print("{}".format(person['name']))
