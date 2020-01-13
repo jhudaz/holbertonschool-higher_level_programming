0460b266b6b3e4b9b46a774399d41f6b7975cf8a
jaime <jandres.vr@hotmail.com>
2020-01-13 15:55:55 +0000

    feature: sends a request to the URL and displays the value of the X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-#
+# sends a request to the URL and displays the value of the X-Request-Id
+from urllib.request import urlopen
+from sys import argv
+# package to handle request and get the arguments from the user
+
+
 if __name__ == "__main__":
+    with urlopen(argv[1]) as response:
+        headers = response.info()
+        if 'X-Request-Id' in headers:
+            print(headers['X-Request-Id'])
