779c37f13cdf3a9ef94486d3244696538b185252
jaime <jandres.vr@hotmail.com>
2020-01-13 16:26:03 +0000

    feature: script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
-#
+# takes in a URL and an email, sends a POST request to the passed URL
+import urllib.parse
+import urllib.request
+from sys import argv
+# package to handle the requests and take the arguments from the user input
+
+
 if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    values = {'email': email}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+        print("{}".format(data.decode()))
