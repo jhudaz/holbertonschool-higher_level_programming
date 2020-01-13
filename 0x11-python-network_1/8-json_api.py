0994ea3b2e1fc1eff8d7d223f21ea81994b126e2
jaime <jandres.vr@hotmail.com>
2020-01-13 21:00:04 +0000

    feature: script post that send a user

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-#
+# script that takes in a letter and sends a POST request to /search_user
+import requests
+from sys import argv
+# packages to handle the request
+
+
 if __name__ == "__main__":
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
 
+    r = requests.post('http://0.0.0.0:5000/search_user', data=q).json()
 
+    if 'id' in r and 'name' in r:
+        print("[{}] {}".format(r['id'], r['name']))
+    else:
+        print("No result")
