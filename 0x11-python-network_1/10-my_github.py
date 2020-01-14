9270b33842740b7fd29dfd504a2c260f6e898e06
jaime <jandres.vr@hotmail.com>
2020-01-13 22:07:26 -0500

    feature: script using github API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
-#
+# script  using github api
+import requests
+from sys import argv
+# package to handle the request
+
+
 if __name__ == "__main__":
+    user = argv[1]
+    passw = argv[2]
+    r = requests.get('https://api.github.com/user', auth=(user, passw)).json()
+    if 'id' in r:
+        print(r['id'])
+    else:
+        print('None')
