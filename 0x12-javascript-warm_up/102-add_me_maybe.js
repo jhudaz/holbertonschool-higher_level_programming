489ea60dfe3435c0266daf16c23ae36107090f06
jaime <jandres.vr@hotmail.com>
2020-02-04 16:57:25 -0500

    fix: changes

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (n, action) {
-  let value = 1;
-  for (let i = 0; i < n; i++) {
-    value++;
-  }
-  return action(value);
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(number + 1);
 };
