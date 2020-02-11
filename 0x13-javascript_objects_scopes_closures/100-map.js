b6bce49f4a884c38bd939e2b17a19bf1d1853720
jaime <jandres.vr@hotmail.com>
2020-02-10 23:14:47 -0500

    feature: map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const list = require("./100-data").list;
+const newList = list.map((value, index) => value * index)
 console.log(list);
-console.log(list.map((value, index) => value * index));
+console.log(newList);
