93dcfc3146a73e23f263dec28293e199855f489c
jaime <jandres.vr@hotmail.com>
2020-02-12 02:32:07 -0500

    fix: amount of lines

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,12 +3,12 @@ const request = require('request');
 
 request(process.argv[2], (err, res, body) => {
   if (err) console.log(err);
-  if (res) {
+  if (res === 200) {
     const response = JSON.parse(body);
     const dict = {};
     response.map(value => (dict[value.userId] = 0));
     response.map(value => {
-      if (value.completed) { dict[value.userId] += 1; }
+      if (value.completed) dict[value.userId] += 1;
     });
     console.log(dict);
   }
