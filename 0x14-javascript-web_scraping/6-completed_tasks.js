2d6d0fe0e0143742743c2ff682a93cf55ec42094
jaime <jandres.vr@hotmail.com>
2020-02-12 01:23:18 -0500

    feature: task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv.slice(2);
+const url = args[0];
+
+const options = {
+  url: url,
+  method: 'GET'
+};
+
+request(options, (err, res, body) => {
+  if (err) console.log(err);
+  if (res) {
+    const response = JSON.parse(body);
+    const ids = [...new Set(response.map(value => value.userId))];
+    const dict = {};
+
+    ids.map(id => {
+      let count = 0;
+      response.map(value => {
+        if (id === value.userId && value.completed) { count++; }
+      });
+      dict[id] = count;
+    });
+    console.log(dict);
+  }
+});
