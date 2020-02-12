e63c205a5dfc5d1d290f236038d49b47d25e1454
jaime <jandres.vr@hotmail.com>
2020-02-11 23:09:43 -0500

    fix: change readFileSync by readFile

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const fs = require('fs');
+const fs = require("fs");
 const args = process.argv.slice(2);
 
-try {
-  const data = fs.readFileSync(args[0], 'utf-8');
-  console.log(data);
-} catch (e) {
-  console.error(e);
-}
+data = fs.readFile(args[0], "utf-8", (err, data) => {
+  if (err) console.error(err);
+  else console.log(data);
+});
