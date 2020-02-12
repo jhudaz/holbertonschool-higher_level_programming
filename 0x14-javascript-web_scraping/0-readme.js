067f3557895657295ea55d411e2e2a5c5e27f51a
jaime <jandres.vr@hotmail.com>
2020-02-11 23:21:58 -0500

    fix console.log

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)[0];
 
-fs.readFile(args[0], 'utf-8', (err, data) => {
-  if (err) console.error(err);
+fs.readFile(args, 'utf-8', (err, data) => {
+  if (err) console.log(err);
   else console.log(data);
 });
