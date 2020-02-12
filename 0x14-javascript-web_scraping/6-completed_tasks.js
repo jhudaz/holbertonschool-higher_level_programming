b78f9a08063965a43db25c6f83902c1b0da946b7
jaime <jandres.vr@hotmail.com>
2020-02-12 02:20:49 -0500

    fix: optimization

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,16 +2,15 @@
 const request = require('request');
 const args = process.argv.slice(2);
 const url = args[0];
+
 request(url, (err, res, body) => {
   if (err) console.log(err);
   if (res) {
     const response = JSON.parse(body);
-    const ids = [...new Set(response.map(value => value.userId))];
     const dict = {};
-    ids.map(id => {
-      let count = 0;
-      response.map(value => id === value.userId && value.completed ? (count++) : (count + 0));
-      dict[id] = count;
+    response.map(value => (dict[value.userId] = 0));
+    response.map(value => {
+      if (value.completed) { dict[value.userId] += 1; }
     });
     console.log(dict);
   }
