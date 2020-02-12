eedd1b104c429269da265dd9bc557fb1999d66bf
jaime <jandres.vr@hotmail.com>
2020-02-11 23:11:51 -0500

    fix: syntax

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 const args = process.argv.slice(2);
 
-data = fs.readFile(args[0], "utf-8", (err, data) => {
+fs.readFile(args[0], 'utf-8', (err, data) => {
   if (err) console.error(err);
   else console.log(data);
 });
