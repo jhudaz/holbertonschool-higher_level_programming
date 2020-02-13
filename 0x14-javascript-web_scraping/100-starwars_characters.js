7e3aae5f9251cdae45d3cc1822025e574077a894
jaime <jandres.vr@hotmail.com>
2020-02-12 23:29:46 -0500

    feature: request star wars api

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,3 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
 const args = process.argv.slice(2);
+const id = args[0];
+
+const options = {
+  url: `https://swapi.co/api/films/${id}`,
+  method: 'GET'
+};
+
+request(options, (err, res, body) => {
+  if (err) console.log(err);
+  if (res) {
+    const response = JSON.parse(body);
+    const characters = response.characters;
+    characters.map(char => {
+      request(char, (err, res, body) => {
+        if (err) console.log(err);
+        if (res) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      }
+      );
+    });
+  }
+});
